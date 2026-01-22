33eb1913498b075d97e2a307b6f3d8a48a5b6b78
이해준 <back2basics@gsrpartner.com>
2026-01-22 13:07:41 +0900

    Refactor login page to use stateful widget and add form validation; integrate animated background and login button

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -1,31 +1,185 @@
-
-import 'package:auto_route/annotations.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_autoroute_shell_app/router/app_router.dart';
 
 @RoutePage()
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final VoidCallback? onLoginCallback;
-
   const LoginPage({super.key, required this.onLoginCallback});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            onLoginCallback?.call();
-            context.router.replaceAll([const AppShellRoute()]);
-          },
-          child: const Text('Login'),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage('assets/images/background.png'), fit: BoxFit.fill),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 30,
+                      width: 80,
+                      height: 200,
+                      child: FadeInUp(
+                        duration: Duration(seconds: 1),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: AssetImage('assets/images/light-1.png')),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 140,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1200),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: AssetImage('assets/images/light-2.png')),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 40,
+                      top: 40,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1300),
+                        child: Container(
+                          decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/images/clock.png'))),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1600),
+                        child: Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(color: Color.fromRGBO(143, 148, 251, .2), blurRadius: 20.0, offset: Offset(0, 10)),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(color: Colors.grey.shade100)),
+                            ),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email or Phone number",
+                                hintStyle: TextStyle(color: Colors.grey[400]),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your email or phone number';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey[400]),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your password';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    FadeInUp(
+                      duration: Duration(milliseconds: 1900),
+                      child: InkWell(
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [Color.fromRGBO(143, 148, 251, 1), Color.fromRGBO(143, 148, 251, .6)],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          widget.onLoginCallback?.call();
+                          context.router.replaceAll([const AppShellRoute()]);
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 70),
+                    FadeInUp(
+                      duration: Duration(milliseconds: 2000),
+                      child: Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1))),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_autoroute_shell_app/app.dart';
@@ -15,7 +14,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   late final SharedPreferences _preferences;
-
+  late double _height, _width;
 
   @override
   void initState() {
@@ -29,7 +28,28 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        _width = constraints.maxWidth;
+        _height = constraints.maxHeight;
+        return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const Text('Settings', style: TextStyle(color: Colors.black)),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Text("Aa", style: TextStyle(color: Colors.green[400])),
+              ),
+            ],
+          ),
+          body: _SettingsWidget(height: _height, width: _width),
+        );
+      },
+    );
+    /*    return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
       ),
@@ -62,6 +82,160 @@ class _SettingsPageState extends State<SettingsPage> {
               }),
             ],
           )
+      ),
+    );*/
+  }
+}
+
+class _SettingsWidget extends StatefulWidget {
+  final double height, width;
+
+  const _SettingsWidget({super.key, required this.height, required this.width});
+
+  @override
+  State<_SettingsWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<_SettingsWidget> {
+  late double height, width;
+
+  @override
+  void initState() {
+    super.initState();
+    height = widget.height;
+    width = widget.width;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildGradient(),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            "Account",
+            style: TextStyle(color: Colors.grey[400], fontSize: 13),
+          ),
+        ),
+        Divider(),
+        _buildItem(
+          "Email",
+          "alice@email.com",
+          Icons.email_outlined,
+          Color(0xFF75abb5),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Text(
+            "General Settings",
+            style: TextStyle(color: Colors.grey[400], fontSize: 13),
+          ),
+        ),
+        Divider(),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  colors: [Color.fromRGBO(143, 148, 251, 1), Color.fromRGBO(143, 148, 251, .6)],
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Log out",
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            onTap: () {
+              final session = SessionScope.of(context);
+              session.isAuthenticated = false;
+
+              context.router.replaceAll([LoginRoute(
+                onLoginCallback: () {
+                  session.isAuthenticated = true;
+                },
+              )]);
+            },
+          ),
+        ),
+        InkWell(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Clear Cache",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+          ),
+          onTap: () async {
+            // context 사용 전 필요한 객체들을 미리 캡처 (async gap 안전)
+            final messenger = ScaffoldMessenger.of(context);
+            final router = context.router;
+            final prefs = await SharedPreferences.getInstance();
+            final successSnack = const SnackBar(content: Text('Cache cleared successfully.'));
+            final failSnack = const SnackBar(content: Text('Failed to clear cache.'));
+            final result = await prefs.clear();
+            if (!mounted) return;
+            messenger.showSnackBar(result ? successSnack : failSnack);
+            router.replaceAll([IntroductionRoute()]);
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildGradient() {
+    return SizedBox(
+      height: height * 0.275,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Color(0xFF75abb5), Color(0xFFafc8cf), Color(0xffd1cdc0)]),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 5.0,
+                  offset: Offset(3.0, 3.0),
+                  color: Colors.black.withValues(alpha: 0.05),
+                  spreadRadius: 4.0,
+                ),
+              ],
+            ),
+            height: height * 0.175,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItem(String text, String text2, IconData icon, Color color) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Row(
+        children: [
+          Icon(icon, color: color),
+          SizedBox(width: 10),
+          Text(text),
+          Spacer(),
+          Text(
+            text2,
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: Colors.grey,
+          ),
+        ],
       ),
     );
   }
